8ccd3a1fb74aeb738332e71464f3ca335ce3a82b
Bradly Chang <bradly@cypone.dev>
2026-09-18 01:30:15 +0800

    fix(ios): avoid iPad window control overlap

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,10 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
-
+  @available(iOS 26.0, *)
+  override func preferredWindowingControlStyle(
+    for windowScene: UIWindowScene
+  ) -> UIWindowScene.WindowingControlStyle {
+    .minimal
+  }
 }
